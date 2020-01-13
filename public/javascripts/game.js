e7a539d8e3c0109ed8c80f3ce63e9f96dd2b5204
Leo Assini <leo.ziyou.assini@gmail.com>
2020-01-13 16:53:46 +0100

    hey

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,8 +1,51 @@
+//var Chess = require('./chess').Chess;
+
+
+var game = new Chess();
+
+
+
 function drop(ev) {
+  ev.preventDefault();
+  var fromId = ev.dataTransfer.getData("start");
+  var from = document.getElementById(fromId);
+  var to = ev.toElement;
+  var toId = to.id;
+  var move = { from: fromId, to: toId }
+  //console.log("drop", ev, ev.toElement.id, from, from.innerHTML, move);
+  var result = game.move(move);
+  if(result==null) return;
+  to.innerHTML = from.innerHTML;
+  from.innerHTML = "";
+} 
+
+
+/* function drop(ev) {
+    console.log("hello");
     console.log(ev);
     ev.preventDefault();
+    var from = ev.dataTransfer.getData("start");
+    console.log(from);
+    var to = ev.toElement.id;
+    console.log(to);
+
+   var res = chess.move({ from: from, to: to });
+
+   if(res==null){
+     console.log("result is null");
+   }
+   else{
+     console.log("movemad");
+   }
+
+  } */
+
+  function drag(ev) {
+    ev.dataTransfer.setData("start", ev.target.id);
   }
 
 function allowDrop(ev){
+  console.log("heythere");
     ev.preventDefault();
 }
+
