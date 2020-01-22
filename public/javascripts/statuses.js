f584a4829fff0a3b1e37e34c7985c6ba8f8c2211
Leo Assini <leo.ziyou.assini@gmail.com>
2020-01-22 13:11:34 +0100

    final?

--- public/javascripts/statuses.js
+++ public/javascripts/statuses.js
@@ -1,10 +1,10 @@
 /* ESLint global variables information */
 /* global Setup */
 var Status = {};
+Status["playAgain"] = " <a href='/play'>Play again!</a>";
 Status["gameWon"] = "Congratulations! You won! " + Status["playAgain"];
 Status["gameLost"] = "Game over. You lost! " + Status["playAgain"];
 Status["gameDraw"] = "It's a draw! " + Status["playAgain"];
-Status["playAgain"] = " <a href='/play'>Play again!</a>";
 Status["aborted"] =
   "Your gaming partner is no longer available, game aborted. " +
   Status["playAgain"];
